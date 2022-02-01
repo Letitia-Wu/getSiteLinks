89b1ddfcd04b7f28aaf13f0b75a100e28d85ff8e
Letitia-Wu <letitia_wood@aliyun.com>
2022-02-01 13:26:44 +0800

    generate a pdf report

--- get-website-info/get_website_info.py
+++ get-website-info/get_website_info.py
@@ -1,93 +1,151 @@
 #!/usr/bin/env python3
 
-import requests, ssl, sys, csv
+import requests, ssl, sys, csv, re, datetime
 from bs4 import BeautifulSoup
-
-# Get the lists of urls and keywords from read_file.py
 from read_file import read_keywords
 from read_file import read_url
-
-import datetime
+from report import generate_report
 from datetime import date
+from reportlab.lib import colors
+from reportlab.lib.units import inch
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
 
-# Turn url_list into a list of lists
-def create_url_lists(url_list):
-    url_lists = []
-    for url in url_list:
-        turn_url_into_list = []
-        turn_url_into_list.append(url)
-        url_lists.append(turn_url_into_list)
-    return url_lists
 
 # Pass in the urls and keywords
 # Iterate through the urls
 # Get <a> tags on each page
 # Iterate through the keywords
 # Get <a> tags containing the keywords
-# Get the href of the <a> tags
-# Add the resulted href to a list
+# Get the href and the text of the <a> tags
+# Add the resulted href and text to a dict
+# Add the dict to a list
 # Add the list to link_list which contains lists of the urls found on each page
-def get_links(url_lists, keywords_list):
-    print("Getting links ...")
-    # a list of lists of urls found on each page
-    link_lists = []
-    for url_list in url_lists:
-        new_url_list = []
-        for url in url_list:
-            #send get request
-            response = requests.get(url)
-            if response.status_code == 404:
-                print("Can't reach {}".format(url))
-            new_url_list.append(url)
-            #parse html page
-            html_page = BeautifulSoup(response.text, "html.parser")
-            # get all <a> tags from the website
-            all_links = html_page.findAll("a")
-            for link in all_links:
-                for keyword in keywords_list:
-                    if keyword in str(link):
-                        href = str(link.get('href'))
-                        if href.endswith("/"):
-                            href = href[:-1]
-                        if "http" not in href:
-                            if href.startswith("/") or href.startswith("#"):
-                                href = url + href
-                            else:
-                                href = url + "/" + href
-                        if href not in new_url_list:
-                            new_url_list.append(href)
-        link_lists.append(new_url_list)
-    print("Got links")
-    return link_lists
 
-def write_txt(keywords_list, link_lists, report_txt):
+def get_links(url_list, keywords_list):
+    print("Getting links ...")
+    # an empty list holding the lists containing dicts
+    link_dict_list = []
+    for url in url_list:
+#    an empty list containing dicts
+        dict_list = []
+        # an empty list which will hold the href, so later we can check whether the href is unique
+        new_url_list = []
+        #send get request
+        response = requests.get(url)
+        if response.status_code == 404:
+            print("Can't reach {}".format(url))
+        new_url_list.append(url)
+        #parse html page
+        html_page = BeautifulSoup(response.text, "html.parser")
+        # get all <a> tags from the website
+        all_links = html_page.findAll("a")
+        for link in all_links:
+        # an empty dict with the current university name, text of the links and href of the links as keys
+            dict = {"university": "", "text": "", "link": ""}
+            for keyword in keywords_list:
+                if keyword in str(link):
+                    href = str(link.get('href'))
+                    if href.endswith("/"):
+                        href = href[:-1]
+                    if "http" not in href:
+                        if href.startswith("/") or href.startswith("#"):
+                            href = url + href
+                        else:
+                            href = url + "/" + href
+                    if href not in new_url_list:
+                        new_url_list.append(href)
+                        text = link.get_text()
+                        dict["university"] = url
+                        dict["text"] = text.strip()
+                        dict["link"] = href.strip()
+                        dict_list.append(dict)
+        link_dict_list.append(dict_list)
+    print("Got links")
+    return link_dict_list
+
+def write_txt(keywords_list, link_dict_list, report_txt):
     print("Writing txt...")
     d = date.today()
     today = d.strftime("%B %d, %Y")
+    keywords_string = ""
+    for keyword in keywords_list:
+        keyword = "'{}' ".format(keyword)
+        keywords_string += keyword
+    
     f = open(report_txt, "w")
-    title = "{} websites searched with the keywords: {}\nGenerated on {}\n\n".format(len(link_lists), " ".join(keywords_list), today)
+    title = "{} universities searched with the keywords: {}\nGenerated on {}\n\n".format(len(link_dict_list), keywords_string, today)
     f.write(title)
-    for link_list in link_lists:
-        f.write("Website searched: {}\n".format(link_list[0]))
+    for link_list in link_dict_list:
+        f.write("\n--------------------------------\n\n")
+        f.write("University: {}\n".format(link_list[0]["university"]))
         f.write("{} results: \n".format(len(link_list)))
-        for link in link_list:
-            f.write(link)
-            f.write("\n")
-        f.write("\n")
+        for dict in link_list:
+            f.write("{}:  {}\n".format(dict["text"], dict["link"]))
     f.close()
     print("TXT written")
-    print("Done")
     return report_txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+def generate_report_info(keywords_list, link_dict_list):
+    styles = getSampleStyleSheet()
+    report_info = ""
+    d = date.today()
+    today = d.strftime("%B %d, %Y")
+    keywords_string = ""
+    for keyword in keywords_list:
+        keyword = "'{}' ".format(keyword)
+        keywords_string += keyword
+    
+    report_info += "Generated on {}".format(today)
+    report_info += "<br/><br/>"
+    report_info += "{} universities searched with the keywords: {}".format(len(link_dict_list), keywords_string)
+    report_info += "<br/><br/>"
+    report_info += "--------------------------------"
+    
+    for link_list in link_dict_list:
+        report_info += "<br/><br/>"
+        report_info += "<b>University: {}</b>\n".format(link_list[0]["university"])
+        report_info += "<br/>"
+        report_info += "{} results: \n".format(len(link_list))
+        report_info += "<br/><br/>"
+        
+        for dict in link_list:
+            report_info += "<h2>{}:  </h2><font color='blue'><u>{}</u></font>".format(dict["text"], dict["link"])
+            report_info += "<br/>"
+        report_info += "<br/><br/>"
+        report_info += "--------------------------------"
+        report_info += "<br/><br/>"
+    return report_info
+
 
 if __name__ == "__main__":
     file_keywords = "keywords.txt"
     file_url = "school_links.txt"
     report_txt = "Websites info.txt"
+    attachment = "Websites report.pdf"
+    title = "Universities Report"
     
     keywords_list = read_keywords(file_keywords)
     url_list = read_url(file_url)
-    url_lists = create_url_lists(url_list)
-
-    link_lists = get_links(url_lists, keywords_list)
-    write_txt(keywords_list, link_lists, report_txt)
+    link_dict_list = get_links(url_list, keywords_list)
+    
+    write_txt(keywords_list, link_dict_list, report_txt)
+    
+    paragraph = generate_report_info(keywords_list, link_dict_list)
+    generate_report(attachment, title, paragraph)
+    
+    print("Done")
 
--- get-website-info/report.py
+++ get-website-info/report.py
@@ -5,13 +5,13 @@ import sys
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
-import datetime
-from datetime import date
 
 def generate_report(attachment, title, paragraph):
+    print("Generating report ...")
     report = SimpleDocTemplate(attachment)
     styles = getSampleStyleSheet()
     report_title = Paragraph(title, styles["h1"])
     empty_line = Spacer(1, 20)
     report_info = Paragraph(paragraph, styles["BodyText"])
-    report.build([report_title, empty_line, report_info, empty_line])
+    report.build([report_title, empty_line, report_info])
+    print("Report generated")
