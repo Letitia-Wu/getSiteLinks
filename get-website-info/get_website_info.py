aef0dd45059bdd34049bc5aad726d5ea27191020
Letitia-Wu <letitia_wood@aliyun.com>
2022-02-05 16:42:51 +0800

    Added a content with innerlinks on pdf

--- get-website-info/get_website_info.py
+++ get-website-info/get_website_info.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests, ssl, sys, csv, re, datetime
+from operator import itemgetter
 from bs4 import BeautifulSoup
 from read_file import read_keywords
 from read_file import read_url
@@ -11,6 +12,7 @@ from reportlab.lib.units import inch
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import PageBreak
 
 
 # Pass in the urls and keywords
@@ -32,11 +34,10 @@ def get_links(url_list, keywords_list):
     for url in url_list:
 #    an empty list containing dicts
         dict_list = []
-        
         #send get request
         response = requests.get(url)
-        if response.status_code == 404:
-            print("Can't reach {}".format(url))
+#        if response.status_code == 404:
+#            print("Can't reach {}".format(url))
         new_url_list.append(url)
         #parse html page
         html_page = BeautifulSoup(response.text, "html.parser")
@@ -59,11 +60,15 @@ def get_links(url_list, keywords_list):
                         new_url_list.append(href)
                         text = link.get_text().replace("\n", "")
                         dict["university"] = url
-                        dict["text"] = text.strip()
+                        if len(text) > 0:
+                            dict["text"] = text.strip()
+                        else:
+                            dict["text"] = "#Couldn't find text for this link"
                         dict["link"] = href.strip()
                         dict_list.append(dict)
         link_dict_list.append(dict_list)
-        print(new_url_list)
+#        for href in new_url_list:
+#            print(href)
     print("Got links")
     return link_dict_list
 
@@ -121,53 +126,62 @@ def write_txt(keywords_list, link_dict_list, report_txt):
     for keyword in keywords_list:
         keyword = "'{}' ".format(keyword)
         keywords_string += keyword
-    
     f = open(report_txt, "w")
+    
     title = "{} universities searched with the keywords: {}\nGenerated on {}\n\n".format(len(link_dict_list), keywords_string, today)
     f.write(title)
     for link_list in link_dict_list:
         f.write("\n--------------------------------\n\n")
         f.write("University: {}\n".format(link_list[0]["university"]))
         f.write("{} results: \n".format(len(link_list)))
-        for dict in link_list:
+        link_list_sorted = sorted(link_list, key=itemgetter('text'))
+        for dict in link_list_sorted:
             f.write("{}:  {}\n".format(dict["text"], dict["link"]))
     f.close()
     print("TXT written")
     return report_txt
 
 def generate_report_info(keywords_list, link_dict_list):
-    styles = getSampleStyleSheet()
+    styles = getSampleStyleSheet()    
     report_info = ""
+    content = ""
     d = date.today()
     today = d.strftime("%B %d, %Y")
     keywords_string = ""
-    
     for keyword in keywords_list:
         keyword = "'{}' ".format(keyword)
         keywords_string += keyword
- 
-    
-    report_info += "Generated on {}".format(today)
+    report_info += "<a name='content'/>Generated on {}".format(today)
     report_info += "<br/><br/>"
     report_info += "{} universities searched with the keywords: {}".format(len(link_dict_list), keywords_string)
     report_info += "<br/><br/>"
-    report_info += "--------------------------------"
+    
+    report_info += "<b>Content</b>"
+    report_info += "<br/><br/>"
     
     for link_list in link_dict_list:
+        number = str(link_dict_list.index(link_list) + 1)
+        content += "{}. {} <a href='#{}' color='green'><i>See result</i></a>".format(number, link_list[0]["university"], link_list[0]["university"])
+        content += "<br/>"
+    report_info += content
+    
+    report_info += "--------------------------------"
+    for link_list in link_dict_list:
         report_info += "<br/><br/>"
-        report_info += "<b>University: {}</b>\n".format(link_list[0]["university"])
+        report_info += "<a name='{}'/><b>University: {}</b>".format(link_list[0]["university"], link_list[0]["university"])
         report_info += "<br/>"
-        report_info += "{} results: \n".format(len(link_list))
+        report_info += "{} results: ".format(len(link_list))
         report_info += "<br/><br/>"
-        
-        for dict in link_list:
+        link_list_sorted = sorted(link_list, key=itemgetter('text'))
+        for dict in link_list_sorted:
             report_info += "{}:  <font color='blue'><u>{}</u></font>".format(dict["text"], dict["link"])
             report_info += "<br/>"
+        report_info += "<br/>"
+        report_info += "<a href='#content'><font color='green'>Back</font></a>"
         report_info += "<br/><br/>"
         report_info += "--------------------------------"
         report_info += "<br/><br/>"
     return report_info
-
 
 if __name__ == "__main__":
     file_keywords = "keywords.txt"
@@ -179,13 +193,11 @@ if __name__ == "__main__":
     keywords_list = read_keywords(file_keywords)
     url_list = read_url(file_url)
     link_dict_list = get_links(url_list, keywords_list)
-#    sublinks_info = get_sublinks(keywords_list, link_dict_list)
 
     write_txt(keywords_list, link_dict_list, report_txt)
     
     paragraph = generate_report_info(keywords_list, link_dict_list)
     generate_report(attachment, title, paragraph)
-    
     
     print("Done")
 
